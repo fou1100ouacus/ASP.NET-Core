4232eea46ba652214059fa8e0069eb1ef1e33b36
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-09-10 03:19:31 +0300

    Add files via upload

---
+++ ModelBinding/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+public class ProductController: ControllerBase
+{
+    
+    [HttpGet("/product-controller")]
+    public IActionResult Get([FromForm] string name, [FromForm] decimal price)
+    {
+        return Ok(new {name, price});
+    }
+
+    [HttpPost("/upload-controller")]
+    public async Task<IActionResult> Post(IFormFile file)
+    {
+       
+        if(file is null || file.Length == 0)
+            return BadRequest("no file uploaded");
+        
+        var uploads= Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        Directory.CreateDirectory(uploads);
+
+        var path = Path.Combine(uploads, file.FileName);
+        using var stream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return Ok("uploaded");
+    }
+}
+
